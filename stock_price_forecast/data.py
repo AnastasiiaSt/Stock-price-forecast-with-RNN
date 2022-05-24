10e4e31bb353c621548586131a8479f3219a2d49
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-24 15:25:34 +0300

    Update data.py

--- stock_price_forecast/data.py
+++ stock_price_forecast/data.py
@@ -3,8 +3,8 @@ import os
 from pathlib import Path
 
 def get_inputs(file_path: str, file_name: str):
-    full_path = os.path.join(Path.cwd(), "file_path", "file_name")
+    full_path = os.path.join(Path.cwd(), file_path, file_name)
     dataset = pd.read_csv(full_path)
     dataset.set_index(pd.to_datetime(dataset["Date"], format="%m/%d/%Y"), inplace = True)
-    dataset.drop(columns = ["Date"], inplace = True)
+    dataset.drop(columns = ["Date", "Adj Close", "Volume"], inplace = True)
     return dataset
