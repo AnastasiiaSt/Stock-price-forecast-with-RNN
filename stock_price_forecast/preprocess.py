cd6c04536ab59398fba63399919193fb7899d88d
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-25 01:13:24 +0300

    Update preprocess.py

--- stock_price_forecast/preprocess.py
+++ stock_price_forecast/preprocess.py
@@ -33,13 +33,17 @@ class Preprocess:
         x = {}
         y = {}
         for price in dataset.columns.to_list():
-            xs, ys = self.x_y_split(dataset[price])
+            xs, indeces_x, ys, indeces_y = self.x_y_split(dataset[price])
             (
                 x[price + "_train"],
                 x[price + "_valid"],
                 y[price + "_train"],
                 y[price + "_valid"],
-            ) = self.train_valid_split(xs, ys)
+                x_train_indeces, 
+                x_valid_indeces,
+                y_train_indeces, 
+                y_valid_indeces
+            ) = self.train_valid_split(xs, indeces_x, ys, indeces_y)
 
         print("Number of time steps in one entry: ", self.t)
         print("Number of predictions in one entry: ", self.p)
@@ -64,30 +68,44 @@ class Preprocess:
         print("Training labels shape: ", y_train.shape)
         print("Validation labels shape: ", y_valid.shape)
 
-        return x_train, y_train, x_valid, y_valid
+        return x_train, y_train, x_valid, y_valid, x_train_indeces, x_valid_indeces, y_train_indeces, y_valid_indeces
 
     def x_y_split(self, dataset):
 
-        dataset = dataset.values.flatten()
+        indeces = dataset.index
+
         w = dataset.shape[0]
         entry = self.t + self.p
         m = w - entry + 1
 
         grouped = dataset[0:entry]
+        grouped_indeces = indeces[0:entry]
         for i in range(1, m):
             grouped = np.vstack([grouped, dataset[i : entry + i]])
+            grouped_indeces = np.vstack([grouped_indeces, indeces[i : entry + i]])
 
         x = grouped[:, 0 : self.t]
+        indeces_x = grouped_indeces[:, 0 : self.t]
         y = grouped[:, (-self.p) :]
+        indeces_y = grouped_indeces[:, (-self.p) :]
 
-        return x, y
+        return x, indeces_x, y, indeces_y
 
-    def train_valid_split(self, x, y):
+    # Convert x shaped (m, t, 4) and y shaped (n, p, 4) into dataset (m + n + t + p, 4)
+
+    def x_y_split_inverse(self, x, y, indeces_x, indeces_y):
+        prices = ["Open", "High", "Low", "Close"]
+        dataset = pd.dataFrame()
+        return dataset
+
+    def train_valid_split(self, x, indeces_x, y, indeces_y):
         valid_number = int(len(x) * self.valid_ratio)
         x_valid, y_valid = x[-valid_number:], y[-valid_number:]
         x_train, y_train = x[:-valid_number], y[:-valid_number]
+        x_valid_indeces, y_valid_indeces = indeces_x[-valid_number:], indeces_y[-valid_number:]
+        x_train_indeces, y_train_indeces = indeces_x[:-valid_number], indeces_y[:-valid_number]
 
-        return x_train, x_valid, y_train, y_valid
+        return x_train, x_valid, y_train, y_valid, x_train_indeces, x_valid_indeces, y_train_indeces, y_valid_indeces
 
     def stack_data(self, dataset):
         stacked = np.dstack
