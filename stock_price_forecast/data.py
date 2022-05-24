aabe86fe68be27b033422e48614c144fd0c47ab8
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-24 03:32:53 +0300

    Module for dataset download is created

---
+++ stock_price_forecast/data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import os
+from pathlib import Path
+
+def get_inputs(file_path: str, file_name: str):
+    full_path = os.path.join(Path.cwd(), "file_path", "file_name")
+    dataset = pd.read_csv(full_path)
+    dataset.set_index(pd.to_datetime(dataset["Date"], format="%m/%d/%Y"), inplace = True)
+    dataset.drop(columns = ["Date"], inplace = True)
+    return dataset
