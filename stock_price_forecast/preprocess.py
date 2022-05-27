ef651ecd6615adcaefb1cc0955ea1e34c6812357
AnastasiiaSt <anastasiiastv@gmail.com>
2022-05-27 23:03:55 +0300

    Update preprocess.py

--- stock_price_forecast/preprocess.py
+++ stock_price_forecast/preprocess.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import numpy as np
+from sklearn.utils import shuffle
 import pandas as pd
 
 
@@ -11,7 +12,7 @@ class Preprocess:
     valid_ratio is ratio of validation set
     """
 
-    def __init__(self, p=5, t=25, valid_ratio=0.15, scaling = "min_max"):
+    def __init__(self, p, t, split, valid_ratio, scaling):
         self.train_dataset = None
         self.mean = None
         self.std = None
@@ -19,17 +20,18 @@ class Preprocess:
         self.x_min = None
         self.p = p
         self.t = t
+        self.split = split
         self.valid_ratio = valid_ratio
         self.scaling = scaling
 
     def fit(self, dataset):
         self.train_dataset = dataset
         if self.scaling == "min_max":
-            self.x_max = np.max(dataset, axis=0)
-            self.x_min = np.min(dataset, axis=0)
+            self.x_max = np.max(dataset.values)
+            self.x_min = np.min(dataset.values)
         elif self.scaling == "standard":
-            self.mean = np.mean(dataset, axis=0)
-            self.std = np.std(dataset, axis=0)
+            self.mean = dataset.mean
+            self.std = dataset.std
         return self
 
     def transform(self, dataset):
@@ -40,50 +42,57 @@ class Preprocess:
             dataset_scaled = x_st * (1 - (0)) + (0)
         elif self.scaling == "standard":
             dataset_scaled = dataset - self.mean / self.std
+        elif self.scaling == "no":
+            pass
+        else:
+            raise ValueError("Defined scaling option is not available" % (self.scaling))
 
-        x = {}
-        y = {}
-        for price in dataset.columns.to_list():
-            xs, indeces_x, ys, indeces_y = self.x_y_split(dataset_scaled[price])
-            (
-                x[price + "_train"],
-                x[price + "_valid"],
-                y[price + "_train"],
-                y[price + "_valid"],
-                x_train_indeces, 
-                x_valid_indeces,
-                y_train_indeces, 
-                y_valid_indeces
-            ) = self.train_valid_split(xs, indeces_x, ys, indeces_y)
+        xs, indeces_x, ys, indeces_y = self.x_y_split(dataset_scaled)
 
         print("Number of time steps in one entry: ", self.t)
         print("Number of predictions in one entry: ", self.p)
         print("Total length of one entry: ", self.t + self.p)
         print("Total number of samples: ", len(xs))
-        print("Number of samples in training set: ", len(x["Close_train"]))
-        print("Number of samples in validation set: ", len(x["Close_valid"]))
 
-        x_train = np.empty((x["Close_train"].shape[0], x["Close_train"].shape[1], 4))
-        x_valid = np.empty((x["Close_valid"].shape[0], x["Close_valid"].shape[1], 4))
-        y_train = np.empty((y["Close_train"].shape[0], y["Close_train"].shape[1], 4))
-        y_valid = np.empty((y["Close_valid"].shape[0], y["Close_valid"].shape[1], 4))
+        if self.split:
+            (
+                x_train,
+                x_valid,
+                y_train,
+                y_valid,
+                x_train_indeces,
+                x_valid_indeces,
+                y_train_indeces,
+                y_valid_indeces,
+            ) = self.train_valid_split(xs, indeces_x, ys, indeces_y)
 
-        for i in range(len(dataset.columns.to_list())):
-            x_train[:, :, i] = x[price + "_train"]
-            x_valid[:, :, i] = x[price + "_valid"]
-            y_train[:, :, i] = y[price + "_train"]
-            y_valid[:, :, i] = y[price + "_valid"]
+            print("Training dataset shape: ", x_train.shape)
+            print("Validation dataset shape: ", x_valid.shape)
+            print("Training labels shape: ", y_train.shape)
+            print("Validation labels shape: ", y_valid.shape)
 
-        print("Training dataset shape: ", x_train.shape)
-        print("Validation dataset shape: ", x_valid.shape)
-        print("Training labels shape: ", y_train.shape)
-        print("Validation labels shape: ", y_valid.shape)
+            return (
+                x_train,
+                y_train,
+                x_valid,
+                y_valid,
+                x_train_indeces,
+                x_valid_indeces,
+                y_train_indeces,
+                y_valid_indeces,
+            )
 
-        return x_train, y_train, x_valid, y_valid, x_train_indeces, x_valid_indeces, y_train_indeces, y_valid_indeces
+        else:
+
+            print("Training dataset shape: ", xs.shape)
+            print("Training labels shape: ", ys.shape)
+
+            return xs, ys, indeces_x, indeces_y
 
     def x_y_split(self, dataset):
 
         indeces = dataset.index
+        dataset = dataset.values.flatten()
 
         w = dataset.shape[0]
         entry = self.t + self.p
@@ -96,28 +105,34 @@ class Preprocess:
             grouped_indeces = np.vstack([grouped_indeces, indeces[i : entry + i]])
 
         x = grouped[:, 0 : self.t]
+        x = x.reshape((x.shape[0], x.shape[1], 1))
         indeces_x = grouped_indeces[:, 0 : self.t]
         y = grouped[:, (-self.p) :]
         indeces_y = grouped_indeces[:, (-self.p) :]
 
         return x, indeces_x, y, indeces_y
 
-    # Convert x shaped (m, t, 4) and y shaped (n, p, 4) into dataset (m + n + t + p, 4)
-
-    def x_y_split_inverse(self, x, y, indeces_x, indeces_y):
-        prices = ["Open", "High", "Low", "Close"]
-        dataset = pd.dataFrame()
-        return dataset
-
     def train_valid_split(self, x, indeces_x, y, indeces_y):
         valid_number = int(len(x) * self.valid_ratio)
-        x_valid, y_valid = x[-valid_number:], y[-valid_number:]
-        x_train, y_train = x[:-valid_number], y[:-valid_number]
-        x_valid_indeces, y_valid_indeces = indeces_x[-valid_number:], indeces_y[-valid_number:]
-        x_train_indeces, y_train_indeces = indeces_x[:-valid_number], indeces_y[:-valid_number]
+        x, y = shuffle(x, y, random_state=42)
+        x_valid, y_valid = x[-valid_number:, :, :], y[-valid_number:, :]
+        x_train, y_train = x[:-valid_number, :, :], y[:-valid_number, :]
+        x_valid_indeces, y_valid_indeces = (
+            indeces_x[-valid_number:],
+            indeces_y[-valid_number:],
+        )
+        x_train_indeces, y_train_indeces = (
+            indeces_x[:-valid_number],
+            indeces_y[:-valid_number],
+        )
 
-        return x_train, x_valid, y_train, y_valid, x_train_indeces, x_valid_indeces, y_train_indeces, y_valid_indeces
-
-    def stack_data(self, dataset):
-        stacked = np.dstack
-        return stacked
+        return (
+            x_train,
+            x_valid,
+            y_train,
+            y_valid,
+            x_train_indeces,
+            x_valid_indeces,
+            y_train_indeces,
+            y_valid_indeces,
+        )
